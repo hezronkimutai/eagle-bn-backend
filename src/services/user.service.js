3538e41ea848649a67d2674c03bea1bcaac3000b
Swaibu Rumbiiha <rumbiihas@gmail.com>
2019-10-29 08:54:02 +0200

    chore(refactor-user-controller):refactor user controller
    - rename user controller
    - add user service folder
    
    [finishes #169392340]

---
+++ src/services/user.service.js
@@ -0,0 +1,44 @@
+import db from '../database/models/index';
+import helpers from '../utils/helper';
+
+const UserService = {
+  async getUserByEmail(email) {
+    const newUser = await db.Users.findOne({
+      where: { email },
+      include: [{ model: db.Roles, attributes: { include: 'roleValue' } }],
+    });
+    return newUser;
+  },
+
+  async updateUserByEmail(updateObj, email) {
+    const newUser = await db.Users.update(updateObj, {
+      where: { email }, returning: true, raw: true
+    });
+    return newUser[1][0];
+  },
+
+  async updateUserById(id, object) {
+    const newUser = await db.Users.update(object, { where: { id } });
+    return newUser;
+  },
+
+  async findOrCreateUser(userData, email) {
+    const [data] = await db.Users.findOrCreate({
+      where: { email },
+      defaults: userData.user
+    });
+    return data;
+  },
+
+  async createUser(fullname, password, email) {
+    const response = await db.Users.create({
+      email,
+      password: helpers.hashPassword(password),
+      fullname
+    });
+    return response;
+  }
+
+};
+
+export default UserService;
