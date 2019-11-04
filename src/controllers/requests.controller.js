4355d435cfe6b5eaa0609515e0f1e4f86eb5ee65
Nshimyumukiza Christian <nshimyumukizachristian@gmail.com>
2019-11-04 19:10:30 +0200

    resolved conflict

--- src/controllers/requests.controller.js
+++ src/controllers/requests.controller.js
@@ -1,24 +1,11 @@
-/* eslint-disable no-restricted-syntax */
-import db from '../database/models';
 import sendResult from '../utils/sendResult';
-import { msg, transporter } from '../config';
-import allRequest from '../utils/requestUtils';
+import RequestService from '../services/request.service';
+import EmailService from '../services/email.service';
 
-const sendMail = (req, res, newRequest) => {
-  const title = `Request ${newRequest.status}`;
-  const url = `/api/v1/requests/${newRequest.id}`;
-  const actionMsg = 'View request';
-  const emailMsg = `Your viewing this message because your request has been ${newRequest.status} at Barefoot nomard`;
-  const message = { ...msg(req, url, title, emailMsg, actionMsg), to: req.user.email };
-  transporter.sendMail(message, () => {
-    sendResult(res, 200, 'updated successfully', newRequest);
-  });
-};
-
-const Request = {
+const RequestsController = {
   async getRequest(req, res) {
     try {
-      const requests = await allRequest.getUserRequest(req.userData.userId);
+      const requests = await RequestService.getAllRequestByUserId(req.userData.userId);
       return sendResult(res, 200, 'Requests', requests);
     } catch (err) {
       return sendResult(res, 400, 'something went wrong!');
@@ -28,31 +15,10 @@ const Request = {
   async postRequest(req, res) {
     try {
       const { country, city, returnTime, trips, timeZone } = req.body;
-      const request = {};
-      request.country = country;
-      request.city = city;
-      request.UserId = req.userData.userId;
-      request.status = 'pending';
-      request.timeZone = timeZone;
-      if (returnTime) request.returnTime = returnTime;
-      let Req = await db.Requests.create(request);
-      Req = Req.get({ plain: true });
-      const Trips = [];
-      let index = 0;
-      for (const trip of trips) {
-        const single = {};
-        single.country = trip.country;
-        single.city = trip.city;
-        single.reason = trip.reason;
-        single.departureTime = trip.departureTime;
-        single.accommodationId = (trip.accommodationId) || null;
-        single.RequestId = Req.id;
-        // eslint-disable-next-line no-await-in-loop
-        Trips[index] = await db.Trips.create(single, { raw: true });
-        index += 1;
-      }
-      Req.trips = Trips;
-      return sendResult(res, 201, 'request created', Req);
+      const { userId } = req.userData;
+      // eslint-disable-next-line max-len
+      const data = await RequestService.createRequest(country, city, returnTime, timeZone, userId, trips);
+      return sendResult(res, 201, 'request created', data);
     } catch (err) {
       return sendResult(res, 400, 'something went wrong!');
     }
@@ -62,8 +28,9 @@ const Request = {
     const { status } = req.params;
     const { request } = req;
     if (request.status === 'pending') {
-      const newRequest = await request.update({ status });
-      return sendMail(req, res, newRequest);
+      const data = await RequestService.updateRequest({ status }, request);
+      EmailService.sendRequestStatusMail(req, data);
+      return sendResult(res, 200, 'updated successfully', data);
     }
     return sendResult(res, 403, 'this request is already approved/rejected');
   },
@@ -75,20 +42,9 @@ const Request = {
   async getManagerRequests(req, res) {
     const { status } = req.query;
     const { managerId } = req.params;
-    const includeUser = { model: db.Users, attributes: ['id', 'email', 'lineManager'], where: { lineManager: managerId } };
-    let requests;
-    if (status) {
-      requests = await db.Requests.findAll({
-        where: { status },
-        include: [includeUser],
-      });
-    } else {
-      requests = await db.Requests.findAll({
-        include: [includeUser]
-      });
-    }
-    sendResult(res, 200, 'request list', requests);
+    const data = await RequestService.getRequestByManagerId(managerId, status);
+    sendResult(res, 200, 'request list', data);
   }
 };
 
-export default Request;
+export default RequestsController;
